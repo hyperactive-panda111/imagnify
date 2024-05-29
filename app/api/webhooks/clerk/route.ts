4a5cbd72409806021406218bb181cb7973dbcccd
Yaw Amo Mensah <amomensah300@gmail.com>
2024-05-29 03:48:07 +0100

    implement error fixes

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -65,10 +65,11 @@ export async function POST(req: Request) {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name || '',
+      lastName: last_name || '',
       photo: image_url,
     };
+    
 
     const newUser = await createUser(user);
 
@@ -89,8 +90,8 @@ export async function POST(req: Request) {
     const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name || '',
+      lastName: last_name || '',
       username: username!,
       photo: image_url,
     };
