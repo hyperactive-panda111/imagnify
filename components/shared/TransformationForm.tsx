2e02554c1f96e093d9b870893cc9cf859342135b
Yaw Amo Mensah <amomensah300@gmail.com>
2024-05-30 11:41:40 +0100

    adding elements to TransformatinForm element

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -42,7 +42,9 @@ export const formSchema = z.object({
 const TransformationForm = ({ action, data = null, userId, type, creditBalance }: TransformationFormProps) => {
     const transformationType = transformationTypes[type];
     const [image, setImage] = useState(data);
-    const [newTransformation, setNewTransformation] = useState<Transformations | null>(null)
+    const [newTransformation, setNewTransformation] = useState<Transformations | null>(null);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [isTransforming, setIsTransforming] = useState(false);
 
     const initalValues = data && action === 'Update' ? {
         title: data?.title,
@@ -71,6 +73,8 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance }
         type: string, onChangeField: (value: string) => void) => {
 
         };
+
+        const onTransformHandler = () => {};
 
   return (
    
@@ -154,7 +158,22 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance }
              )} 
         </div>
        )}
-       <Button type='submit'>Submit</Button>
+
+       <div className='flex flex-col gap-4'>
+       <Button
+         type='button'
+         className='submit-button capitalize'
+         disabled={isTransforming || newTransformation === null}
+         onClick={onTransformHandler} 
+        >{isTransforming ? 'Transforming...' : 'Apply Transformation'}</Button>
+        
+        <Button
+         type='submit'
+         className='submit-button capitalize'
+         disabled={isSubmitting}          
+        >{isSubmitting ? 'Submitting...' : 'Save Image'}</Button>
+       
+       </div>
       </form>
     </Form>
   )
