1c0fc54723cabdd8a092e4a61953ac97fbdf7101
Yaw Amo Mensah <amomensah300@gmail.com>
2024-05-30 20:55:55 +0100

    added files

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -19,12 +19,14 @@ const AddTransformationTypePage = async ({ params: {type} }: SearchParamProps) =
       subtitle={transformation.subTitle}
     />
 
+    <section className='mt-10'>
     <TransformationForm 
       action='Add'
       userId={user._id}
       type={transformation.type as TransformationTypeKey}
       creditBalance={user.creditBalance}
       />
+    </section>
     </>
     
   )
