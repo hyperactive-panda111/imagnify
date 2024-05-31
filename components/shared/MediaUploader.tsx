7d804a42f18c9ca9eccf1da958601db3960b3e24
Yaw Amo Mensah <amomensah300@gmail.com>
2024-05-31 02:40:04 +0100

    added image upload and toaster

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -1,6 +1,10 @@
+'use client';
+
 import Image from 'next/image';
 import { useToast } from '../ui/use-toast';
-import { CldUploadWidget } from 'next-cloudinary';
+import { CldImage, CldUploadWidget } from 'next-cloudinary';
+import { dataUrl, getImageSize } from '@/lib/utils';
+import { PlaceholderValue } from 'next/dist/shared/lib/get-img-props';
 
 type MediaUploaderProps = {
     onValueChange: (value: string) => void;
@@ -20,6 +24,16 @@ const MediaUploader = ({
     const { toast } = useToast();
     
     const onUploadSuccessHandler = (result: any) => {
+        setImage((prevState: any) => ({
+            ...prevState,
+            publicId: result?.info?.public_id,
+            width: result?.info?.width,
+            height: result?.info?.height,
+            secureUrl: result?.info?.secure_url
+        }))
+
+        onValueChange(result?.info?.public_id)
+
         toast({
             title: 'Image uploaded successfully',
             description: '1 credit was deducted from your account',
@@ -53,7 +67,18 @@ const MediaUploader = ({
                 </h3>
                 {publicId ? (
                     <>
-                    HERE IS THE IMAGE
+                      <div className='cursor-pointer 
+                      overflow-hidden rounded-[10px]'>
+                        <CldImage
+                          width={getImageSize(type, image, 'width')}
+                          height={getImageSize(type, image, 'height')}
+                          src={publicId}
+                          alt='image'
+                          sizes={'(max-width: 767px) 100vw, 50vw'}
+                          placeholder={dataUrl as PlaceholderValue}
+                          className='media-uploader_cldImage'
+                        />
+                     </div>
                     </>
                 ) : (
                     <div className='media-uploader_cta'
