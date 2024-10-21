48f9015de817c3e0b91e4da37eb4c1a5739c2bb1
amomensah675 <amomensah675@gmail.com>
2024-10-21 00:51:17 +0000

    change authMiddleware(deprecated) for clerkMiddleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,8 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
-,
-ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)", "/transformations/",],
-})
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
